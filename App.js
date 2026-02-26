7955e970cd330c5b8e5a8864eec2214d29b7314b
aluno <juanpietro.oliveira2019@gmail.com>
2026-02-26 09:41:38 -0300

    .

--- App.js
+++ App.js
@@ -6,10 +6,15 @@ import {
   TouchableOpacity,
   StyleSheet,
   SafeAreaView,
-  Image
+  Image,
+  SafeAreaViewBase
 } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useRoute } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
 
-function App() {
+function Login({navigation}) {
   return (
     <SafeAreaView style={styles.container}>
       
@@ -43,18 +48,18 @@ function App() {
         />
 
 
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity onPress={()=>navigation.navigate('Lista_contato')} style={styles.button}>
           <Text style={styles.buttonText}>Logar</Text>
         </TouchableOpacity>
 
 
-        <TouchableOpacity style={styles.buttonSecondary}>
+        <TouchableOpacity onPress={()=>navigation.navigate('Cadastro')} style={styles.buttonSecondary}>
           <Text style={styles.buttonText}>Cadastre-se</Text>
         </TouchableOpacity>
 
 
         <TouchableOpacity>
-          <Text style={styles.forgotPassword}>esqueceu a senha</Text>
+          <Text onPress={()=>navigation.navigate('Esqueceu_senha')} style={styles.forgotPassword}>esqueceu a senha</Text>
         </TouchableOpacity>
 
       </View>
@@ -63,7 +68,7 @@ function App() {
   );
 }
 
-function cadastro() {
+function Cadastro({navigation}) {
   return (
     <SafeAreaView style={styles.container}>
       
@@ -77,6 +82,12 @@ function cadastro() {
         <TextInput
           style={styles.input}
           placeholder="Digite seu nome"
+        />
+
+        <Text style={styles.label}>Cpf</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Digite seu Cpf"
         />
 
         <Text style={styles.label}>Email</Text>
@@ -103,7 +114,7 @@ function cadastro() {
 }
 
 
-export default function esqueceu_senha() {
+function Esqueceu_senha({navigation}) {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
@@ -129,6 +140,120 @@ export default function esqueceu_senha() {
   );
 }
 
+function Contato({navigation}) {
+  return (
+    <SafeAreaView style={styles.container}>
+      
+      
+      <View style={styles.header}>
+        <Text style={styles.title}>Contato</Text>
+      </View>
+      <View style={styles.card}>
+
+        <Text style={styles.label}>Nome</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Digite seu nome"
+        />
+
+        <Text style={styles.label}>Email</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Digite seu email"
+          keyboardType="email-address"
+        />
+
+        <Text style={styles.label}>Telefone</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Digite sua senha"
+          secureTextEntry
+        />
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Alterar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Excluir</Text>
+        </TouchableOpacity>
+      </View>
+
+    </SafeAreaView>
+  );
+}
+
+
+function Lista_contato({navigation, route}){
+  return(
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+        <Text style={styles.titleContatos}>Lista de Contatos</Text>
+      </View>
+
+      <View style={styles.card}>
+        
+
+        <View style={styles.avatarContainer}>
+          <View style={styles.avatar} />
+        </View>
+
+
+        <Text style={styles.label}>Email</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Digite seu email"
+          keyboardType="email-address"
+        />
+
+
+        <Text style={styles.label}>Senha</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Digite sua senha"
+          secureTextEntry
+        />
+
+</View>
+      </SafeAreaView>
+
+      
+  );
+
+  
+}
+
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+return (
+<NavigationContainer>
+<Stack.Navigator initialRouteName="Login">
+<Stack.Screen name="Login" component={Login} />
+<Stack.Screen name="Cadastro" component={Cadastro} />
+<Stack.Screen name="Esqueceu_senha" component={Esqueceu_senha} />
+<Stack.Screen name="Contato" component={Contato} />
+<Stack.Screen name="Lista_contato" component={Lista_contato}
+options={{
+    title: "Lista de Contatos",
+    headerRight: () => (
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Contato")}
+        style={{ marginRight: 15 }}
+      >
+        <Ionicons name="add" size={28} color="black" />
+      </TouchableOpacity>
+    ),
+  }} />
+</Stack.Navigator>
+</NavigationContainer>
+);
+}
+
+export default App;
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -142,9 +267,16 @@ const styles = StyleSheet.create({
     top: 50,
   },
   title: {
-    color: '#fff',
+    color: '#000000',
     fontSize: 16,
     fontWeight: 'bold',
+  },
+  titleContatos: {
+    color: '#000000',
+    fontSize: 16,
+    fontWeight: 'bold',
+    width: '100%',
+    height: 45, 
   },
   card: {
     width: '85%',
